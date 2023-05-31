35137abf219ee76b204cf56d743ae4c04166734a
Felipe Nava <felipe.nava.co@gmail.com>
2023-05-30 18:36:58 -0700

    refactor: clean up not needed changes

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,7 +6,6 @@
 
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
-
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
