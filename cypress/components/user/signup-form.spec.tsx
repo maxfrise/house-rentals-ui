549fa0cbc60a8ee660a403e72cfe971663979a6f
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-01 14:47:57 -0700

    feat: adding sign up dialog

--- cypress/components/user/signup-form.spec.tsx
+++ cypress/components/user/signup-form.spec.tsx
@@ -1,38 +1,36 @@
 import React from "react";
 
-import { LoginForm } from "../../../app/components/user/";
+import { SignUpForm } from "../../../app/components/user/";
 import { render } from '../../../cypress/support/render';
 
-describe("<LoginForm />", () => {
-  it("Should render LoginForm", () => {
-    render(<LoginForm />);
+describe("<SignUpForm />", () => {
+  it("Should render SignUpForm", () => {
+    render(<SignUpForm />);
 
     cy.findByRole('form').should('be.visible');
     cy.findByRole('textbox', { name: 'Correo electronico' }).should('be.visible');
     cy.findByLabelText('Contraseña').should('be.visible');
-    cy.findByRole('button', { name: 'Iniciar sesion' }).should('be.visible');
+    cy.findByRole('button', { name: 'Crear cuenta' }).should('be.visible');
   });
 
-  it("Should render back button if onBackClick is provided", () => {
-    const mockedFn = cy.stub().as('backClickSpy');
-    render(<LoginForm onBackClick={mockedFn} />);
+  it("Should render back button", () => {
+    const mockedFn = cy.stub().as('backFn');
+    render(<SignUpForm onBackClick={mockedFn} />);
 
     cy.findByRole('form').should('be.visible');
     cy.findByRole('textbox', { name: 'Correo electronico' }).should('be.visible');
     cy.findByLabelText('Contraseña').should('be.visible');
-    cy.findByRole('button', { name: 'Iniciar sesion' }).should('be.visible');
-
+    cy.findByRole('button', { name: 'Crear cuenta' }).should('be.visible');
     cy.findByRole('button', { name: 'Regresar' }).as('backBtn').should('be.visible');
-    
-    cy.get('@backBtn').click();
 
-    cy.get('@backClickSpy').should('have.been.calledOnce');
+    cy.get('@backBtn').click();
+    cy.get('@backFn').should('have.been.calledOnce');
   });
 
-  it("Should redirect to Houses when login is correct", () => {
-    render(<LoginForm />, [
+  it("Should redirect to Houses when sign up is correct", () => {
+    render(<SignUpForm />, [
       {
-        path: '/login',
+        path: '/join',
         action: async () => {
           return {
             errors: null,
@@ -40,7 +38,7 @@ describe("<LoginForm />", () => {
           }
         },
         loader: async () => { return { ok: true } },
-        element: <p>Login route</p>
+        element: <p>Sign up route</p>
       },
       {
         path: '/houses',
@@ -53,17 +51,17 @@ describe("<LoginForm />", () => {
     cy.findByRole('form').should('be.visible');
     cy.findByRole('textbox', { name: 'Correo electronico' }).type('dev@dev.com');
     cy.findByLabelText('Contraseña').type('123456');
-    cy.findByRole('button', { name: 'Iniciar sesion' }).click();
+    cy.findByRole('button', { name: 'Crear cuenta' }).click();
 
     cy.findByText('Houses route').should('be.visible');
   });
 
-  it("Should show error message when login is NOT correct", () => {
-    render(<LoginForm />, [
+  it("Should show error message when sign up is NOT correct", () => {
+    render(<SignUpForm />, [
       {
-        path: '/login',
+        path: '/join',
         action: async () => {
-          return { errors: { email: "Email o password invalido", password: null }, userId: null }
+          return { errors: { email: "El correo ya existe", password: null }, userId: null }
         },
         loader: async () => { return { ok: true } },
         element: <p>Login route</p>
@@ -73,8 +71,8 @@ describe("<LoginForm />", () => {
     cy.findByRole('form').should('be.visible');
     cy.findByRole('textbox', { name: 'Correo electronico' }).type('dev@dev.com');
     cy.findByLabelText('Contraseña').type('123456');
-    cy.findByRole('button', { name: 'Iniciar sesion' }).click();
+    cy.findByRole('button', { name: 'Crear cuenta' }).click();
 
-    cy.findByText('Email o password invalido').should('be.visible');
+    cy.findByText('El correo ya existe').should('be.visible');
   });
 });
