3d3a46bef78f9969c42aa847709ca8a3c9d4458b
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-08 16:07:43 -0700

    refactor: clean up

--- app/components/dashboard/navbar.tsx
+++ app/components/dashboard/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams, useResolvedPath } from '@remix-run/react';
+import { Link, useParams } from '@remix-run/react';
 
 import styled from 'styled-components';
 
