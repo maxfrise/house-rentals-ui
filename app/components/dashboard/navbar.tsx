5b0433b3b7e15857a1ebb261370f9fe7dece76af
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-08 20:29:00 -0700

    refactor: use UiNavbar (#63)
    * refactor: use UiNavbar
    
    * refactor: clean up

--- app/components/dashboard/navbar.tsx
+++ app/components/dashboard/navbar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from '@remix-run/react';
+import { Link, useParams } from '@remix-run/react';
 
 import styled from 'styled-components';
 
 import { UiCard } from '@uireact/card';
-import { UiList, UiListItem } from '@uireact/list';
 import type { UiSpacingProps } from '@uireact/foundation';
 import { UiSpacing , TextSize} from '@uireact/foundation';
 import { UiIcon } from '@uireact/icons';
 import { UiBadge } from '@uireact/badge';
+import { UiText } from '@uireact/text';
+import { UiNavbar, UiNavbarItem } from '@uireact/navbar';
 
 import type { House } from '../../types';
-import { UiText } from '@uireact/text';
+import { UiFlexGrid, UiFlexGridItem } from '@uireact/flex-grid';
 
 type NavbarProps = {
   houses?: House[];
@@ -29,51 +30,61 @@ const Div = styled.div`
 const StyledLink = styled(Link)`
   display: inline-block;
   width: 100%;
+  padding: 5px;
 `
 
 const navbarSpacing: UiSpacingProps['margin'] = { block: 'three' };
 const homeLinkSpacing: UiSpacingProps['margin'] = { left: 'four' };
 
-export const Navbar: React.FC<NavbarProps> = ({ houses }: NavbarProps) => (
-  <Div>
-    <>
-      <UiCard>
-        <StyledLink to="/houses">
-          <UiSpacing margin={homeLinkSpacing}>
-            <UiIcon icon='Home' /> Inicio
+export const Navbar: React.FC<NavbarProps> = ({ houses }: NavbarProps) => {
+  const { houseId } = useParams();
+
+  return (
+    <Div>
+      <>
+        <UiCard>
+          <StyledLink to="/houses">
+            <UiSpacing margin={homeLinkSpacing}>
+              <UiIcon icon='Home' /> Inicio
+            </UiSpacing>
+          </StyledLink>
+        </UiCard>
+          <UiSpacing margin={navbarSpacing}>
+            <UiCard noPadding>
+            {houses && houses.length > 0 ? (
+              <UiNavbar orientation='stacked'>
+                {houses?.map((house, index) => (
+                  <UiNavbarItem key={index} active={houseId === house.houseId}>
+                    <StyledLink to={house.houseId}>
+                      <UiFlexGrid>
+                        <UiFlexGridItem grow={1}>
+                          {`${house.houseFriendlyName} `}
+                        </UiFlexGridItem>
+                        <UiFlexGridItem>
+                          {house.leaseStatus === 'AVAILABLE' ? (
+                            <UiBadge category='primary' size={TextSize.xsmall}>Disponible</UiBadge>
+                          ) : (
+                            <UiBadge category='positive' size={TextSize.xsmall}>Rentada</UiBadge>
+                          )}
+                        </UiFlexGridItem>
+                      </UiFlexGrid>
+                    </StyledLink>
+                  </UiNavbarItem>
+                ))}
+              </UiNavbar>
+            ) : (
+            <UiText>Todavia no hay casas</UiText>   
+            )}
+            </UiCard>
           </UiSpacing>
-        </StyledLink>
-      </UiCard>
-        <UiSpacing margin={navbarSpacing}>
-          <UiCard>
-          {houses && houses.length > 0 ? (
-            <UiList>
-              {houses?.map((house, index) => (
-                <UiListItem key={index}>
-                  {(index > 0 && index === houses.length - 1) && <hr />}
-                  <StyledLink to={house.houseId}>
-                    {`${house.houseFriendlyName} `}
-                    {house.leaseStatus === 'AVAILABLE' ? (
-                      <UiBadge category='primary' size={TextSize.xsmall}>Disponible</UiBadge>
-                    ) : (
-                      <UiBadge category='positive' size={TextSize.xsmall}>Rentada</UiBadge>
-                    )}
-                  </StyledLink>
-                </UiListItem>
-              ))}
-            </UiList>
-          ) : (
-           <UiText>Todavia no hay casas</UiText>   
-          )}
-          </UiCard>
-        </UiSpacing>
-      <UiCard>
-        <StyledLink to="./new">
-          <UiSpacing margin={homeLinkSpacing}>
-            <UiIcon icon='Pages' /> Agregar casa
-          </UiSpacing>
-        </StyledLink>
-      </UiCard>
-    </>
-  </Div>
-);
+        <UiCard>
+          <StyledLink to="./new">
+            <UiSpacing margin={homeLinkSpacing}>
+              <UiIcon icon='Pages' /> Agregar casa
+            </UiSpacing>
+          </StyledLink>
+        </UiCard>
+      </>
+    </Div>
+  );
+};
